0f89c53aef21a9848d267538a40f463981d86b13
MehranESB <mehranesmaily907@gmail.com>
2025-04-07 10:28:18 +0330

    create python dataset objects for single and mulit time frame datasets

---
+++ create_single_multi_dataset.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+from chartDL.utils.csv import import_ohlcv_from_csv
+from chartDL.dataset import MultiDataset, SingleDataset
+
+# create single time frame dataset
+file_path = Path("./DATA/csv/EURUSD-15m.csv")
+df = import_ohlcv_from_csv(file_path, header=True, datetime_format="%Y-%m-%d %H:%M:%S")
+dataset = SingleDataset(df, seq_len=32)
+
+# create mutli time frame dataset
+multi_folder_path = Path("./DATA/multi_view/EURUSD-1h")
+multi_dataset = MultiDataset(
+    multi_folder_path, seq_len=32, from_to=["2020.04.01 16:30", "2022.10.01 00:00"]
+)
+
+print(f"single dataset in path: {file_path}")
+print(dataset[100])
+
+# pick a sample and convert and print on screen
+print(f"multi time frame dataset in folder path: {multi_folder_path}")
+mulit_data = multi_dataset[20]
+for data in mulit_data:
+    print(f"timeframe: {data.timeframe}")
+    print(data.to_dataframe().head())
